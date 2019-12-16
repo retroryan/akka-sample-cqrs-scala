e7c653c9879117f96175f3739a93173dbca60e33
Ryan Knight <ryan@grandcloud.com>
2019-12-16 10:10:43 -0700

    converting stream to future seq

--- src/main/scala/sample/cqrs/ShoppingCartRoutes.scala
+++ src/main/scala/sample/cqrs/ShoppingCartRoutes.scala
@@ -1,11 +1,7 @@
 package sample.cqrs
 
 import akka.NotUsed
-
-import scala.concurrent.Future
-import scala.concurrent.duration._
-import akka.actor.typed.ActorRef
-import akka.actor.typed.ActorSystem
+import akka.actor.typed.{ActorRef, ActorSystem}
 import akka.cluster.sharding.typed.scaladsl.ClusterSharding
 import akka.http.scaladsl.common.{EntityStreamingSupport, JsonEntityStreamingSupport}
 import akka.http.scaladsl.model.StatusCodes
@@ -13,13 +9,19 @@ import akka.http.scaladsl.server.Route
 import akka.persistence.cassandra.query.scaladsl.CassandraReadJournal
 import akka.persistence.query.{NoOffset, PersistenceQuery}
 import akka.stream.scaladsl
+import akka.stream.scaladsl.Sink
 import akka.util.Timeout
 import spray.json.DefaultJsonProtocol
 
+import scala.concurrent.Future
+
 
 object ShoppingCartRoutes {
+
   final case class AddItem(cartId: String, itemId: String, quantity: Int)
+
   final case class UpdateItem(cartId: String, itemId: String, quantity: Int)
+
 }
 
 class ShoppingCartRoutes()(implicit system: ActorSystem[_]) {
@@ -28,10 +30,10 @@ class ShoppingCartRoutes()(implicit system: ActorSystem[_]) {
     Timeout.create(system.settings.config.getDuration("shopping.askTimeout"))
   private val sharding = ClusterSharding(system)
 
+  import JsonFormats._
   import ShoppingCartRoutes._
   import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
   import akka.http.scaladsl.server.Directives._
-  import JsonFormats._
 
   val shopping: Route =
     pathPrefix("shopping") {
@@ -70,6 +72,7 @@ class ShoppingCartRoutes()(implicit system: ActorSystem[_]) {
           },
           path("all") {
             get {
+              //THIS NEVER COMPLETES - It appears the stream never completes?
               import akka.actor.typed.scaladsl.adapter._
               val queries = PersistenceQuery(system.toClassic)
                 .readJournalFor[CassandraReadJournal](CassandraReadJournal.Identifier)
@@ -85,17 +88,23 @@ class ShoppingCartRoutes()(implicit system: ActorSystem[_]) {
                       case _ => None
                     }
                 }.collect {
-                case Some(itemAdded) => itemAdded
+                case Some(itemAdded) => {
+                  system.log.info(s"Collecting Item Added: ${itemAdded.cartId}")
+                  itemAdded
+                }
               }
 
               implicit val jsonStreamingSupport: JsonEntityStreamingSupport =
                 EntityStreamingSupport.json()
 
+              //Akka Stream never completes ?
               complete(shoppingCarts)
             }
           },
           path("nothing") {
             get {
+              //This works fine to log shopping cart events by tag to the console
+              //This just uses run for each on the query stream but doesn't return anything to the client
               getShoppingCartsForTag(ShoppingCart.CART_OPENED)
               getShoppingCartsForTag(ShoppingCart.CART_ADJUSTED)
               getShoppingCartsForTag(ShoppingCart.CART_REMOVED)
@@ -106,22 +115,30 @@ class ShoppingCartRoutes()(implicit system: ActorSystem[_]) {
           path("future") {
             get {
 
+              //Try and query the stream and collect everything into a future seq.
               import akka.actor.typed.scaladsl.adapter._
               val queries = PersistenceQuery(system.toClassic)
                 .readJournalFor[CassandraReadJournal](CassandraReadJournal.Identifier)
 
-              queries
+              val futureQueryResults: Future[Seq[ShoppingCart.ItemAdded]] = queries
                 .eventsByTag(ShoppingCart.CART_OPENED, NoOffset)
-                .runForeach {
+                .map {
                   eventEnvelope =>
                     eventEnvelope.event match {
                       case shoppingCartItemAdded: ShoppingCart.ItemAdded =>
                         system.log.info(s"Shopping Cart ID: ${shoppingCartItemAdded.cartId} Item ID: ${shoppingCartItemAdded.itemId} Quantity: ${shoppingCartItemAdded.quantity}")
-                      case event =>
-                        system.log.info(s"Event Type: $event")
+                        Some(shoppingCartItemAdded)
+                      case _ => None
                     }
+                }.collect {
+                case Some(itemAdded) => {
+                  system.log.info(s"Collecting Item Added: ${itemAdded.cartId}")
+                  itemAdded
                 }
-              complete(StatusCodes.OK -> "Logged Shopping Carts")
+              }.runWith(Sink.seq)
+
+
+              complete(StatusCodes.OK -> futureQueryResults)
             }
           },
           pathPrefix(Segment) { cartId =>
@@ -148,7 +165,7 @@ class ShoppingCartRoutes()(implicit system: ActorSystem[_]) {
     }
 
 
-  private def getShoppingCartsForTag(tag:String): Unit = {
+  private def getShoppingCartsForTag(tag: String): Unit = {
     import akka.actor.typed.scaladsl.adapter._
     val queries = PersistenceQuery(system.toClassic)
       .readJournalFor[CassandraReadJournal](CassandraReadJournal.Identifier)
@@ -177,7 +194,6 @@ object JsonFormats extends DefaultJsonProtocol {
 
   import spray.json.RootJsonFormat
   // import the default encoders for primitive types (Int, String, Lists etc)
-  import spray.json.DefaultJsonProtocol._
 
   implicit val summaryFormat: RootJsonFormat[ShoppingCart.Summary] = jsonFormat2(ShoppingCart.Summary)
   implicit val addItemFormat: RootJsonFormat[ShoppingCartRoutes.AddItem] = jsonFormat3(ShoppingCartRoutes.AddItem)
